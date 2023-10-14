4cd3f296f139010730d72b3605e7f9365afc91b4
lendl-uy <jluy@smartfoxdata.com>
2023-10-14 09:32:06 +0800

    Created verify_q() to reduce redundant code. Added get_column_norm().

--- hw3/QR_Decomposition.py
+++ hw3/QR_Decomposition.py
@@ -78,6 +78,18 @@ def get_rank(A):
 
     return len(independent_vec_idx), independent_vec_idx
 
+def get_column_norm(Q):
+    
+    norm = []
+    
+    m, n = Q.shape
+    
+    for col in range(n):
+        norm.append(np.linalg.norm(Q[:, col]))
+        
+    print(f"norm = {norm}")
+    return norm
+
 # TODO: Finish implementing null space calculator
 def get_null_space(A, Q, rank, independent_vecs):
     
@@ -88,23 +100,25 @@ def get_null_space(A, Q, rank, independent_vecs):
                 
     # Get the null space based on the basis and the index of 
     # independent vectors
-    for col in range(n):
+    for col in range(m):
         
         if col not in independent_vecs:
             
-            for row in range(n):
+            for row in range(m):
                 if row not in independent_vecs:
                     Q[row, col] = -1
                 elif row in independent_vecs and row <= col:
                     Q[row, col] = rref_A[row, col]
                 else:
                     Q[row, col] = 0
+   
+    print(f"Q after null space = {Q}")
                                             
 def qr_decomposition(A, epsilon=1e-8):
 
     # Get the mxn shape of the matrix
     m, n = A.shape
-    
+    print(f"A = {A}")
     # Initialize matrices Q, R, and P
     Q = np.zeros((m, m))
     R = np.zeros((m, n))
@@ -116,12 +130,6 @@ def qr_decomposition(A, epsilon=1e-8):
     # which vectors to perform Gram-Schmidt 
     rank, independent_vec_idx = get_rank(A.copy())
 
-    # If rank is less than number of rows, get the null space of A
-    if rank < m:
-        
-        # Get the null space of A and update Q
-        get_null_space(A.copy(), Q, rank, independent_vec_idx)
-
     # Perform Gram-Schmidt algorithm for getting orthonormal vectors
     for col in range(rank):
         
@@ -132,32 +140,49 @@ def qr_decomposition(A, epsilon=1e-8):
         # Entries of Q only spans the column space of A
         if col >= m:
             break
+        
+        # Perform column pivoting if column vector entries are all zeros
+        # Swap with column with the highest absolute value of entries
+        if np.sum(A[:, col]) < epsilon:
+            col_vec = np.absolute(A[:, col])
+            swap_idx = col+np.where(col_vec == np.max(col_vec))[0][0]
+            A[:, col], A[:, swap_idx] = A[:, swap_idx], A[:, col].copy()
+            P[:, col], P[:, swap_idx] = P[:, swap_idx], P[:, col].copy()
 
-        for row in range(m):
+        # Obtain projection vector p
+        p = np.zeros(m)
+        for i in range(pivot_count):
+            dot_prod = np.dot(A[:, pivot_count], Q[:, i])
+            p += dot_prod*Q[:, i]
 
-            mtx_elem = A[row, col]
+        e = A[:, pivot_count] - p # Obtain orthogonal vector e = a - p
+        q = e*1/np.linalg.norm(e) # Normalize e to get q_i
+        Q[:, pivot_count] = q
+        pivot_count += 1
+        break
+    
+    column_norm = get_column_norm(A.copy())
 
-            # Perform column pivoting if entry is less than specified epsilon
-            if abs(mtx_elem) < epsilon:
-
-                col_vec = np.absolute(A[row, col:])
-                swap_idx = col+np.where(col_vec == np.max(col_vec))[0][0]
-                A[:, col], A[:, swap_idx] = A[:, swap_idx], A[:, col].copy()
-                P[:, col], P[:, swap_idx] = P[:, swap_idx], P[:, col].copy()
-
-            if abs(mtx_elem) > epsilon:
-                # Obtain projection vector p
-                p = np.zeros(m)
-                for i in range(pivot_count):
-                    dot_prod = np.dot(A[:, pivot_count], Q[:, i])
-                    p += dot_prod*Q[:, i]
-
-                e = A[:, pivot_count] - p # Obtain orthogonal vector e = a - p
-                q = e*1/np.linalg.norm(e) # Normalize e to get q_i
-                Q[:, pivot_count] = q
-                pivot_count += 1
-                break
+    # If rank is less than number of columns, get the null space of A
+    if rank < n:
+        
+        # Get the null space of A and update Q
+        get_null_space(A.copy(), Q, rank, independent_vec_idx)
 
     R = np.transpose(Q) @ A
 
     return Q, R, P
+
+def verify_qr(A, Q, R, P):
+    
+    # Verification of computed matrices
+    AP = A @ P
+    QR = Q @ R
+    
+    m, n = A.shape
+    
+    # allclose() is used due to loss of precision when performing row-wise operations
+    if np.allclose(np.subtract(AP, QR), np.zeros((m, n), dtype="float"), rtol=1e-3, atol=1e-5):
+        print("Decomposition is correct!")
+    else:
+        raise RuntimeError("The decomposition is incorrect!")
--- hw3/QR_Decomposition_Tests.py
+++ hw3/QR_Decomposition_Tests.py
@@ -6,35 +6,9 @@
 
 from QR_Decomposition import *
 
-def qr_decomposition_of_matrix_with_full_rank():
+def qr_decomposition_of_square_matrix_full_rank():
     
-    print("---- Running QR decomposition test for matrix with full rank ----")
-    
-    A = np.array([[1, 2, 3],
-                  [4, 5, 6],
-                  [7, 8, 9]]).astype("float")
-    
-    # Pass a copy of A instead since Python will modify the original contents of A
-    Q, R, P = qr_decomposition(A.copy(), EPSILON)
-    
-    print("Results:")
-    print(f"P = {P}\n")
-    print(f"Q = {Q}\n")
-    print(f"R = {R}\n")
-    
-    # Verification of computed matrices
-    AP = A @ P
-    QR = Q @ R
-    
-    # allclose() is used due to loss of precision when performing row-wise operations
-    if np.allclose(np.subtract(AP, QR), np.zeros((3, 3), dtype="float"), rtol=1e-3, atol=1e-5):
-        print("Decomposition is correct!")
-    else:
-        raise RuntimeError("Something went wrong with the decomposition!")
-    
-def qr_decomposition_of_matrix_with_rank_less_than_columns():
-    
-    print("---- Running QR decomposition test for matrix with rank < columns ----")
+    print("---- Running QR decomposition test for square matrix with full rank ----")
     
     A = np.array([[1, 2, 3],
                   [4, 5, 6],
@@ -48,23 +22,53 @@ def qr_decomposition_of_matrix_with_rank_less_than_columns():
     print(f"Q = {Q}\n")
     print(f"R = {R}\n")
     
-    # Verification of computed matrices
-    AP = A @ P
-    QR = Q @ R
+    verify_qr(A, Q, R, P)
     
-    # allclose() is used due to loss of precision when performing row-wise operations
-    if np.allclose(np.subtract(AP, QR), np.zeros((3, 3), dtype="float"), rtol=1e-3, atol=1e-5):
-        print("Decomposition is correct!")
-    else:
-        raise RuntimeError("Something went wrong with the decomposition!")
+def qr_decomposition_of_square_matrix_rank_deficient():
     
-def qr_decomposition_of_matrix_with_rank_greater_than_columns():
+    print("---- Running QR decomposition test for square matrix that is rank-deficient ----")
     
-    print("---- Running QR decomposition test for matrix with rank > columns ----")
+    A = np.array([[1, 2, 3],
+                  [4, 5, 6],
+                  [7, 8, 9]]).astype("float")
+    
+    # Pass a copy of A instead since Python will modify the original contents of A
+    Q, R, P = qr_decomposition(A.copy(), EPSILON)
+    
+    print("Results:")
+    print(f"P = {P}\n")
+    print(f"Q = {Q}\n")
+    print(f"R = {R}\n")
+    
+    verify_qr(A, Q, R, P)
+    
+def qr_decomposition_of_nonsquare_matrix_with_rows_greater_than_columns():
+    
+    print("---- Running QR decomposition test for non-square matrix where m > n ----")
+    
+    A = np.array([[1, 2, 3],
+                  [4, 5, 6],
+                  [7, 8, 9],
+                  [10, 11, 12],
+                  [13, 14, 15]]).astype("float")
+    
+    # Pass a copy of A instead since Python will modify the original contents of A
+    Q, R, P = qr_decomposition(A.copy(), EPSILON)
+    
+    print("Results:")
+    print(f"P = {P}\n")
+    print(f"Q = {Q}\n")
+    print(f"R = {R}\n")
+    
+    verify_qr(A, Q, R, P)
+    
+def qr_decomposition_of_nonsquare_matrix_with_columns_greater_than_rows():
+    
+    print("---- Running QR decomposition test for non-square matrix where n > m ----")
     
     A = np.array([[1, 3, 0, 0, 3],
                   [0, 0, 1, 0, 9],
-                  [0, 0, 0, 1, 4]]).astype("float")
+                  [0, 0, 0, 1, -4]]).astype("float")
     
     # Pass a copy of A instead since Python will modify the original contents of A
     Q, R, P = qr_decomposition(A.copy(), EPSILON)
@@ -78,11 +82,7 @@ def qr_decomposition_of_matrix_with_rank_greater_than_columns():
     AP = A @ P
     QR = Q @ R
     
-    # allclose() is used due to loss of precision when performing row-wise operations
-    if np.allclose(np.subtract(AP, QR), np.zeros((3, 5), dtype="float"), rtol=1e-3, atol=1e-5):
-        print("Decomposition is correct!")
-    else:
-        raise RuntimeError("Something went wrong with the decomposition!")
+    verify_qr(A, Q, R, P)
     
 def qr_decomposition_random_mtx(NUM_TESTS):
     
@@ -101,20 +101,16 @@ def qr_decomposition_random_mtx(NUM_TESTS):
         AP = A @ P
         QR = Q @ R
 
-        # allclose() is used due to loss of precision when performing row-wise operations
-        if np.allclose(np.subtract(AP, QR), np.zeros((m, n), dtype="float"), rtol=1e-3, atol=1e-5):
-            #print("Decomposition is correct!")
-            continue
-        else:
-            raise RuntimeError("Something went wrong with the decomposition!")
+        verify_qr(A, Q, R, P)
         
     print(f"All decomposition tests are correct!")
 
 def main():
 
-    #qr_decomposition_of_matrix_with_full_rank()
-    #qr_decomposition_of_matrix_with_rank_less_than_columns()
-    qr_decomposition_of_matrix_with_rank_greater_than_columns()
+    #qr_decomposition_of_square_matrix_full_rank()
+    #qr_decomposition_of_square_matrix_rank_deficient()
+    qr_decomposition_of_nonsquare_matrix_with_rows_greater_than_columns()
+    #qr_decomposition_of_nonsquare_matrix_with_columns_greater_than_rows()
     #qr_decomposition_random_mtx(1000)
 
 if __name__ == "__main__":
