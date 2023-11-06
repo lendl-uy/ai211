21064e802c44a19ff61fa29a5394065bedca96b8
lendl-uy <jluy@smartfoxdata.com>
2023-11-06 21:45:52 +0800

    Added randomized testing for bidiagonalization and tridiagonalization.

--- hw6/Do_SVD_Tests.py
+++ hw6/Do_SVD_Tests.py
@@ -14,11 +14,11 @@ def perform_bidiagonalization_3x3_matrix():
                   [5, 1, 4],
                   [0, 4, 3]]).astype("float")
 
-    print(f"A = {A}")
+    print(f"A = {A}\n")
     
     B = get_bidiagonal_matrix(A)
     
-    print("\nResults:\n")
+    print("Results:\n")
     print(f"B = {B}\n")
     
     verify_bidiagonal_matrix(A, B)
@@ -31,7 +31,14 @@ def perform_tridiagonalization_3x3_matrix():
                   [5, 1, 4],
                   [0, 4, 3]]).astype("float")
 
-    print(f"A = {A}")
+    print(f"A = {A}\n")
+        
+    T = get_tridiagonal_matrix(A)
+    
+    print("Results:\n")
+    print(f"T = {T}\n")
+    
+    verify_tridiagonal_matrix(A, T)
     
 def perform_bidiagonalization_on_matrix_with_random_sizes_and_entries(NUM_TESTS):
     
@@ -44,12 +51,35 @@ def perform_bidiagonalization_on_matrix_with_random_sizes_and_entries(NUM_TESTS)
         
         #print(f"A = {A}")
         
+        B = get_bidiagonal_matrix(A)
+        
+        verify_bidiagonal_matrix(A, B, suppress_success_flag=True)
+        
     print(f"All computed bidiagonal matrices are correct for {NUM_TESTS} test matrices!")
+    
+def perform_tridiagonalization_on_matrix_with_random_sizes_and_entries(NUM_TESTS):
+    
+    print(f"\n---- Performing {NUM_TESTS} tridiagonalization tests on matrices with random sizes and entries ----\n")
+    
+    for i in range(NUM_TESTS):
+        # Initialize matrix A to be decomposed
+        m = np.random.randint(3, 50) # Random number of rows
+        A = np.random.randint(-10, 10, size=(m, m)).astype("float")
+        
+        #print(f"A = {A}")
+        
+        T = get_tridiagonal_matrix(A)
+        
+        verify_tridiagonal_matrix(A, T, suppress_success_flag=True)
+
+    print(f"All computed tridiagonal matrices are correct for {NUM_TESTS} test matrices!")
 
 def main():
     
     perform_bidiagonalization_3x3_matrix()
-    #perform_bidiagonalization_on_matrix_with_random_sizes_and_entries(100)
+    perform_tridiagonalization_3x3_matrix()
+    perform_bidiagonalization_on_matrix_with_random_sizes_and_entries(100000)
+    perform_tridiagonalization_on_matrix_with_random_sizes_and_entries(100000)
 
 if __name__ == "__main__":
     main()
