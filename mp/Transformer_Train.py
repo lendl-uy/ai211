6d2d71a53854cd44e8a4f85ea43bf49ee91d7255
cayasryan <rrcayas@gmail.com>
2024-01-21 13:06:26 +0800

    added code to save params

--- mp/Transformer_Train.py
+++ mp/Transformer_Train.py
@@ -4,6 +4,7 @@
 
 import numpy as np
 from tqdm import tqdm
+import pickle
 import matplotlib.pyplot as plt
 
 
@@ -108,3 +109,61 @@ plt.ylabel('Loss')
 plt.title('Training and Testing Loss Over Epochs')
 plt.legend()
 plt.show()
+
+# Save model parameters
+transformer_params = {
+    "train_losses": train_losses,
+    "test_losses": test_losses,
+    "epochs": EPOCHS,
+    "batch_size": BATCH_SIZE,
+    "train_percentage": TRAIN_PERCENTAGE,
+    "sentence_length": SENTENCE_LENGTH,
+    "max_seq_length": MAX_SEQ_LENGTH,
+    "learning_rate": LEARNING_RATE,
+    "d_model": D_MODEL,
+    "d_ff": D_FF,
+    "heads": HEADS,
+    "enc_vocab_token_to_index": enc_vocab.token_to_index,
+    "enc_vocab_index_to_token": enc_vocab.index_to_token,
+    "dec_vocab_token_to_index": dec_vocab.token_to_index,
+    "dec_vocab_index_to_token": dec_vocab.index_to_token,
+    "src_vocab_embedding": model.source_embedding_layer.vocab_embedding,
+    "src_pos_embed": model.source_pos_embedding_layer.pos_embedding, 
+    "tgt_vocab_embedding": model.target_embedding_layer.vocab_embedding,
+    "tgt_pos_embed": model.target_pos_embedding_layer.pos_embedding,
+    "enc_W_q": model.encoder.mha_layer.W_q,
+    "enc_W_k": model.encoder.mha_layer.W_k,
+    "enc_W_v": model.encoder.mha_layer.W_v,
+    "enc_W_o": model.encoder.mha_layer.W_o,
+    "enc_norm1_beta": model.encoder.norm_layer_1.beta,
+    "enc_norm1_gamma": model.encoder.norm_layer_1.gamma,
+    "enc_ff_weights1": model.encoder.ff_layer.weights_1,
+    "enc_ff_biases1": model.encoder.ff_layer.biases_1,
+    "enc_ff_weights2": model.encoder.ff_layer.weights_2,
+    "enc_ff_biases2": model.encoder.ff_layer.biases_2,
+    "enc_norm2_gamma": model.encoder.norm_layer_2.gamma,
+    "enc_norm2_beta": model.encoder.norm_layer_2.beta,
+    "dec_masked_W_q": model.decoder.mmha_layer.W_q,
+    "dec_masked_W_k": model.decoder.mmha_layer.W_k,
+    "dec_masked_W_v": model.decoder.mmha_layer.W_v,
+    "dec_masked_W_o": model.decoder.mmha_layer.W_o,
+    "dec_norm1_gamma": model.decoder.norm_layer_1.gamma,
+    "dec_norm1_beta": model.decoder.norm_layer_1.beta,
+    "dec_W_q": model.decoder.mha_layer.W_q,
+    "dec_W_k": model.decoder.mha_layer.W_k,
+    "dec_W_v": model.decoder.mha_layer.W_v,
+    "dec_W_o": model.decoder.mha_layer.W_o,
+    "dec_norm2_gamma": model.decoder.norm_layer_2.gamma,
+    "dec_norm2_beta": model.decoder.norm_layer_2.beta,
+    "dec_ff_weights1": model.decoder.ff_layer.weights_1,
+    "dec_ff_biases1": model.decoder.ff_layer.biases_1,
+    "dec_ff_weights2": model.decoder.ff_layer.weights_2,
+    "dec_ff_biases2": model.decoder.ff_layer.biases_2,
+    "dec_norm3_gamma": model.decoder.norm_layer_3.gamma,
+    "dec_norm3_beta": model.decoder.norm_layer_3.beta,
+    "final_linear_weights": model.output_linear_layer.weights,
+    "final_linear_bias": model.output_linear_layer.bias
+    }
+
+with open('transformer_params.pkl', 'wb') as file:
+    pickle.dump(transformer_params, file)
